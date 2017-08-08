a7a58f9215764346820a97b5e7cd33a5bc50bd0d
Joseph Gabito <codehaiku.io@gmail.com>
2017-08-08 14:29:04 +0800

    Fixed header bug in blank page template

--- page-templates/blank.php
+++ page-templates/blank.php
@@ -2,10 +2,22 @@
 /**
  * Template Name: Blank
  */
-
-//get_header();
-wp_head();
-
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php  if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
+    <link rel="icon" href="<?php echo esc_url( thrive_favicon_url() ); ?>" type="image/x-icon" />
+	<link rel="shortcut icon" href="<?php echo esc_url( thrive_favicon_url() ); ?>" type="image/x-icon" />
+<?php } ?>
+<?php wp_head(); ?>
+</head>
+<body <?php body_class( array('thrive-inline') ); ?>>
+<?php
 if( have_posts() ){
 	while( have_posts() ){
 		the_post();
@@ -13,7 +25,8 @@ if( have_posts() ){
 		// inorder for page builder to work, you need the page builder's shortcode
 		// right into the textarea wherein you compose your blog
 		?>
-	<?php do_action( 'thrive_before_page_content' ); ?>
+		
+		<?php do_action( 'thrive_before_page_content' ); ?>
 
 		<div class="row">
 			<div id="blank-template-content" class="col-md-12">
@@ -33,6 +46,6 @@ if( have_posts() ){
 	}
 }
 
-wp_footer();
-//get_footer();
-?>
+wp_footer(); ?>
+</body>
+</html>
