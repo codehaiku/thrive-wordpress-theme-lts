87f34350a6859968dfeee62e63d75d84e0c20ad9
jasperjardin <jasper.jardin1994@gmail.com>
2018-04-05 20:26:27 +0800

    Added a folder to contain the files that holds the custom functions for BuddyPress.

---
+++ inc/bp-functions/thrive-bp-nav.php
@@ -0,0 +1,188 @@
+<?php
+/**
+* Rearrange the array base on the parameter given.
+*
+* @since 2.2.0
+*
+* @return array Rearrange array.
+*/
+function thrive_array_sort_by_column( &$arr, $col, $dir = SORT_ASC ) {
+    $sort_col = array();
+
+    foreach ($arr as $key=> $row) {
+        $sort_col[$key] = $row[$col];
+    }
+
+    return array_multisort($sort_col, $dir, $arr);
+}
+
+/**
+* Wrapper for the BP component count.
+*
+* @since 2.2.0
+*
+* @return string HTML count wrapper.
+*/
+function thrive_component_notification_wrapper( $count ) {
+    $class = '';
+    if ( empty( $count ) || is_null ( $count ) ) {
+        $count = 0;
+    }
+    $class = ( 0 === $count ) ? 'no-count' : 'count';
+
+    return '<span class="' . $class . '">' . $count . '</span>';
+}
+
+/**
+* Fetch the Primary BP Menus.
+*
+* @since 2.2.0
+*
+* @return array $bp_filtered_components_menu List of BP primary menus.
+*/
+function thrive_bp_component_nav_setup() {
+    if ( !function_exists('buddypress') ) {
+        return;
+    }
+
+    $bp = buddypress();
+    $logged_in_id = bp_loggedin_user_id();
+    $bp_active_components = $bp->active_components;
+    $bp_active_components_loaded = $bp->loaded_components;
+    $bp_components_menu = array();
+    $bp_filtered_components_menu = array();
+    $component_name = '';
+
+    foreach ( $bp_active_components_loaded as $component => $value ) {
+        $component_name = $bp->bp_nav[$component]['name'];
+        $bp->bp_nav[$component]['component_name'] = $component;
+
+        if ( 'messages' === $component ) {
+            $user_total_messages = BP_Messages_Thread::get_inbox_count( $logged_in_id );
+            $bp->bp_nav[$component]['component_notifications'] = thrive_component_notification_wrapper( $user_total_messages );
+        }
+        if ( 'notifications' === $component ) {
+            $user_total_notifications = thrive_component_notification_wrapper( bp_notifications_get_unread_notification_count( $logged_in_id ) );
+            $bp->bp_nav[$component]['component_notifications'] = $user_total_notifications;
+        }
+        if ( 'friends' === $component ) {
+            $user_total_friends = thrive_component_notification_wrapper( friends_get_total_friend_count( $logged_in_id ) );
+            $bp->bp_nav[$component]['component_notifications'] = $user_total_friends;
+        }
+        if ( 'groups' === $component ) {
+            $user_total_groups = thrive_component_notification_wrapper( groups_total_groups_for_user( $logged_in_id ) );
+            $bp->bp_nav[$component]['component_notifications'] = $user_total_groups;
+        }
+        if ( 'projects' === $component ) {
+            if( class_exists( 'TaskBreakerCore' ) ) {
+                $taskbreaker = new TaskBreakerCore();
+                $taskbreaker_user_total_projects = $taskbreaker->get_user_groups_projects( $logged_in_id );
+
+                $bp->bp_nav[$component]['component_notifications'] = thrive_component_notification_wrapper( $taskbreaker_user_total_projects['total'] );
+            }
+        }
+        $bp->bp_nav[$component]['component_label'] = preg_replace("#(<span.*?>)(.*?)(</span>)#", "", $component_name) . $bp->bp_nav[$component]['component_notifications'];
+
+        if ( ! is_null( $bp->bp_nav[$component] ) ) {
+            $bp_components_menu[] = $bp->bp_nav[$component];
+        }
+    }
+    foreach ( $bp_components_menu as $menu) {
+        if( true === $menu['primary'] ) {
+            $bp_filtered_components_menu[] = $menu;
+        }
+    }
+
+    thrive_array_sort_by_column( $bp_filtered_components_menu, 'position' );
+
+    return $bp_filtered_components_menu;
+}
+
+/**
+* Fetch the Secondary BP Menus.
+*
+* @since 2.2.0
+*
+* @return void List of BP secondary menus markup.
+*/
+function thrive_bp_get_sub_nav( $parent_slug = '' ) {
+    if ( !function_exists('buddypress') ) {
+        return;
+    }
+
+    $bp = buddypress();
+    $sub_nav = array();
+    $class = '';
+
+    // If we are looking at a member profile, then the we can use the current
+    // component as an index. Otherwise we need to use the component's root_slug.
+    $component_index = !empty( bp_loggedin_user_id() ) ? bp_current_component() : bp_get_root_slug( bp_current_component() );
+    $selected_item = bp_current_action();
+
+    // Default to the Members nav.
+    if ( ! bp_is_single_item() ) {
+        // Set the parent slug, if not provided.
+        if ( empty( $parent_slug ) ) {
+            $parent_slug = $component_index;
+        }
+
+        $secondary_nav_items = $bp->members->nav->get_secondary( array( 'parent_slug' => $parent_slug ) );
+
+        if ( ! $secondary_nav_items ) {
+            return false;
+        }
+
+    // For a single item, try to use the component's nav.
+    } else {
+        $current_item = bp_current_item();
+        $single_item_component = bp_current_component();
+
+        // Adjust the selected nav item for the current single item if needed.
+        if ( ! empty( $parent_slug ) ) {
+            $current_item = $parent_slug;
+            $selected_item = bp_action_variable( 0 );
+        }
+
+        // If the nav is not defined by the parent component, look in the Members nav.
+        if ( ! isset( $bp->{$single_item_component}->nav ) ) {
+            $secondary_nav_items = $bp->members->nav->get_secondary( array( 'parent_slug' => $current_item ) );
+        } else {
+            $secondary_nav_items = $bp->{$single_item_component}->nav->get_secondary( array( 'parent_slug' => $current_item ) );
+        }
+
+        if ( ! $secondary_nav_items ) {
+            return false;
+        }
+    }
+
+    // Loop through each navigation item.
+    foreach ( $secondary_nav_items as $subnav_item ) {
+
+        $class = '';
+
+        // If the current action or an action variable matches the nav item id, then add a highlight CSS class.
+        if ( $selected_item == $subnav_item->slug ) {
+            $class .= 'current-menu-item';
+        } else {
+            $class .= '';
+        }
+
+        $sub_nav['name'] = $subnav_item->name;
+        $sub_nav['slug'] = $subnav_item->slug;
+        $sub_nav['parent'] = $subnav_item->parent_slug;
+        $sub_nav['css_id'] = $subnav_item->css_id;
+        $sub_nav['class'] = $class;
+
+        $sub_nav['link'] = bp_loggedin_user_domain() . $sub_nav['parent'] . '/' . $sub_nav['slug'];
+
+        ?>
+        <li id="<?php echo esc_attr( $sub_nav['css_id'] ) ?>" class="menu-child <?php echo esc_attr( $sub_nav['class'] ) ?>">
+            <a href="<?php echo esc_url( $sub_nav['link'] ) ?>">
+                <?php echo esc_html( $sub_nav['name'] ) ?>
+            </a>
+        </li>
+        <?php
+    }
+
+    return;
+}
