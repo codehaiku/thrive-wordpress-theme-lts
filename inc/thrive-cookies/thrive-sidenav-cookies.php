bd90abe6f3024e98c97d9fe9f6671667c32562f5
Joseph Gabito <codehaiku.io@gmail.com>
2018-04-18 11:35:29 +0800

    new updates

--- inc/thrive-cookies/thrive-sidenav-cookies.php
+++ inc/thrive-cookies/thrive-sidenav-cookies.php
@@ -6,16 +6,18 @@
 add_action( 'init', 'thrive_set_sidenav_cookie' );
 
 function thrive_set_sidenav_cookie() {
+
     $sidenav_state = '';
     $sidenav_cookie = '';
     $filtered_sidenav_cookie = '';
     $set_sidenav_cookie = '';
+    $is_sidenavigation_closed = filter_input( INPUT_COOKIE, 'isSideNavSidebarClose', FILTER_SANITIZE_SPECIAL_CHARS );
 
-    if ( empty( $_COOKIE['isSideNavSidebarClose'] ) ) {
-        $_COOKIE['isSideNavSidebarClose'] = apply_filters( 'thrive_set_default_sidenav_state', __return_false() );
+    if ( empty( $is_sidenavigation_closed ) ) {
+       $is_sidenavigation_closed = apply_filters( 'thrive_set_default_sidenav_state', __return_false() );
     }
 
-    $sidenav_cookie = $_COOKIE['isSideNavSidebarClose'];
+    $sidenav_cookie = $is_sidenavigation_closed;
 
     $filtered_sidenav_cookie = filter_var( $sidenav_cookie, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE );
 
@@ -26,15 +28,18 @@ function thrive_set_sidenav_cookie() {
 }
 
 function thrive_get_sidenav_state() {
+    
     $sidenav_state = '';
     $sidenav_cookie = '';
     $filtered_sidenav_cookie = '';
 
-    if ( empty( $_COOKIE['isSideNavSidebarClose'] ) ) {
-        $_COOKIE['isSideNavSidebarClose'] = apply_filters( 'thrive_get_default_sidenav_state', __return_false() );
+    $is_sidenavigation_closed = filter_input( INPUT_COOKIE, 'isSideNavSidebarClose', FILTER_SANITIZE_SPECIAL_CHARS );
+
+    if ( empty( $is_sidenavigation_closed ) ) {
+        $is_sidenavigation_closed = apply_filters( 'thrive_get_default_sidenav_state', __return_false() );
     }
 
-    $sidenav_cookie = $_COOKIE['isSideNavSidebarClose'];
+    $sidenav_cookie = $is_sidenavigation_closed;
 
     $filtered_sidenav_cookie = filter_var( $sidenav_cookie, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE );
 
