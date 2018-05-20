6bff69387f4839fe8ae0d677a4a30123b238a354
Joseph Gabito <codehaiku.io@gmail.com>
2018-05-20 17:18:06 +0800

    new bbpress update

--- buddypress/members/single/home.php
+++ buddypress/members/single/home.php
@@ -15,44 +15,33 @@ do_action( 'template_notices' );
 
 		<?php
 
-		/**
+				/**
 		 * Fires before the display of member body content.
 		 *
-		 * @since BuddyPress (1.2.0)
+		 * @since 1.2.0
 		 */
 		do_action( 'bp_before_member_body' );
-
-		if ( bp_is_user_activity() || !bp_current_component() ) :
+		if ( bp_is_user_front() ) :
+			bp_displayed_user_front_template_part();
+		elseif ( bp_is_user_activity() ) :
 			bp_get_template_part( 'members/single/activity' );
-
 		elseif ( bp_is_user_blogs() ) :
 			bp_get_template_part( 'members/single/blogs'    );
-
 		elseif ( bp_is_user_friends() ) :
 			bp_get_template_part( 'members/single/friends'  );
-
 		elseif ( bp_is_user_groups() ) :
 			bp_get_template_part( 'members/single/groups'   );
-
 		elseif ( bp_is_user_messages() ) :
 			bp_get_template_part( 'members/single/messages' );
-
 		elseif ( bp_is_user_profile() ) :
 			bp_get_template_part( 'members/single/profile'  );
-
-		elseif ( bp_is_user_forums() ) :
-			bp_get_template_part( 'members/single/forums'   );
-
 		elseif ( bp_is_user_notifications() ) :
 			bp_get_template_part( 'members/single/notifications' );
-
 		elseif ( bp_is_user_settings() ) :
 			bp_get_template_part( 'members/single/settings' );
-
 		// If nothing sticks, load a generic template
 		else :
 			bp_get_template_part( 'members/single/plugins'  );
-
 		endif;
 
 		/**
